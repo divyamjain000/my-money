59ec50237750da5a57f165d9765d94074c912242
Chirag agarwal <chirag.agarwal2907@gmail.com>
2022-11-12 16:58:43 +0530

    files added

---
+++ src/components/Navbar.js
@@ -0,0 +1,112 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function NavBar() {
+  return (
+    <div>
+      {/* <!-- Navbar --> */}
+      {/* <!-- Navbar --> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {/* <!-- Container wrapper --> */}
+        <div className="container-fluid"> 
+          {/* <!-- Toggle button --> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+
+          {/* <!-- Collapsible wrapper --> */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {/* <!-- Navbar brand --> */}
+            <a className="navbar-brand mt-2 mt-lg-0" href="#">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+                height="15"
+                alt="MDB Logo"
+                loading="lazy"
+              />
+            </a>
+            {/* <!-- Left links --> */}
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Dashboard
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Expenses
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+            {/* <!-- Left links --> */}
+          </div>
+          {/* <!-- Collapsible wrapper --> */}
+
+          {/* <!-- Right elements --> */}
+          <div className="d-flex align-items-center">
+            {/* <!-- Icon --> */}
+            <a className="text-reset me-3" href="#">
+              <i className="fas fa-shopping-cart"></i>
+            </a>
+
+            {/* <!-- Avatar --> */}
+            <div className="dropdown">
+              <a
+                className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                href="#"
+                id="navbarDropdownMenuAvatar"
+                role="button"
+                data-mdb-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                  className="rounded-circle"
+                  height="25"
+                  alt="Black and White Portrait of div Man"
+                  loading="lazy"
+                />
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="navbarDropdownMenuAvatar"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    My profile
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* <!-- Right elements --> */}
+        </div>
+        {/* <!-- Container wrapper --> */}
+      </nav>
+      {/* <!-- Navbar --> */}
+      {/* <!-- Navbar --> */}
+    </div>
+  );
+}
